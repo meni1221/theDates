f00ff5c5490a5931d5b3cdbc8017c820e86863c0
Meni <menil@MENI.com>
2024-07-24 12:22:07 +0300

    Add project files.

---
+++ theDates/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace theDates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.cmbDay = new System.Windows.Forms.ComboBox();
+            this.cmbDayOfTheMonth = new System.Windows.Forms.ComboBox();
+            this.cmbMonth = new System.Windows.Forms.ComboBox();
+            this.cmbYear = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Guttman Drogolin", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(306, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "כתיבת תאריך";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Guttman Drogolin", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(559, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "היום בשבוע";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Guttman Drogolin", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(406, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "היום בחודש";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Guttman Drogolin", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(292, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "חודש";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Guttman Drogolin", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label5.Location = new System.Drawing.Point(177, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 27);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "שנה";
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Font = new System.Drawing.Font("Guttman Drogolin", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblresult.Location = new System.Drawing.Point(378, 330);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(0, 27);
+            this.lblresult.TabIndex = 5;
+            // 
+            // cmbDay
+            // 
+            this.cmbDay.FormattingEnabled = true;
+            this.cmbDay.Items.AddRange(new object[] {
+            "ראשון",
+            "שני",
+            "שלישי",
+            "רביעי",
+            "חמישי",
+            "שישי",
+            "שבת"});
+            this.cmbDay.Location = new System.Drawing.Point(545, 136);
+            this.cmbDay.Name = "cmbDay";
+            this.cmbDay.Size = new System.Drawing.Size(121, 28);
+            this.cmbDay.TabIndex = 6;
+            // 
+            // cmbDayOfTheMonth
+            // 
+            this.cmbDayOfTheMonth.FormattingEnabled = true;
+            this.cmbDayOfTheMonth.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30"});
+            this.cmbDayOfTheMonth.Location = new System.Drawing.Point(400, 136);
+            this.cmbDayOfTheMonth.Name = "cmbDayOfTheMonth";
+            this.cmbDayOfTheMonth.Size = new System.Drawing.Size(121, 28);
+            this.cmbDayOfTheMonth.TabIndex = 7;
+            // 
+            // cmbMonth
+            // 
+            this.cmbMonth.FormattingEnabled = true;
+            this.cmbMonth.Items.AddRange(new object[] {
+            "תשרי",
+            "חשוון",
+            "כסלו",
+            "טבת",
+            "שבט",
+            "אדר",
+            "אדר הראשון",
+            "אדר השני",
+            "ניסן",
+            "אייר",
+            "סיון",
+            "תמוז",
+            "אב",
+            "אלול"});
+            this.cmbMonth.Location = new System.Drawing.Point(263, 136);
+            this.cmbMonth.Name = "cmbMonth";
+            this.cmbMonth.Size = new System.Drawing.Size(121, 28);
+            this.cmbMonth.TabIndex = 8;
+            // 
+            // cmbYear
+            // 
+            this.cmbYear.FormattingEnabled = true;
+            this.cmbYear.Items.AddRange(new object[] {
+            "תשפ\"ד",
+            "תשפ\"ה",
+            "תשפ\"ו",
+            "תשפ\"ז",
+            "תשפ\"ח",
+            "תשפ\"ט",
+            "תש\"צ",
+            "תשצ\"א",
+            "תשצ\"ב",
+            "תשצ\"ג",
+            "תשצ\"ד"});
+            this.cmbYear.Location = new System.Drawing.Point(136, 136);
+            this.cmbYear.Name = "cmbYear";
+            this.cmbYear.Size = new System.Drawing.Size(121, 28);
+            this.cmbYear.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(136, 221);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(133, 54);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "כתיבה";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cmbYear);
+            this.Controls.Add(this.cmbMonth);
+            this.Controls.Add(this.cmbDayOfTheMonth);
+            this.Controls.Add(this.cmbDay);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblresult;
+        private System.Windows.Forms.ComboBox cmbDay;
+        private System.Windows.Forms.ComboBox cmbDayOfTheMonth;
+        private System.Windows.Forms.ComboBox cmbMonth;
+        private System.Windows.Forms.ComboBox cmbYear;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ theDates/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//יבוא של ספריית XML
+using System.Xml;
+using static System.Windows.Forms.LinkLabel;
+using System.Xml.Linq;
+using System.Collections;
+
+namespace theDates
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument xmlDocument; // משתנה לאחסון מסמך ה-XML
+        string pathName = Directory.GetCurrentDirectory() + "\\theDatesData.xml"; // מחרוזת שמייצגת את הנתיב לקובץ ה-XML
+            //יצירת אלמנט בשם תוצאה
+            XmlNode result;
+        public Form1()
+        {
+            InitializeComponent();
+            xmlDocument = new XmlDocument(); // יצירת אובייקט XML חדש
+
+            if (File.Exists(pathName)) // בדיקה אם קובץ ה-XML קיים
+            {
+                try
+                {
+                    xmlDocument.Load(pathName); // טעינת קובץ ה-XML אם קיים
+                }
+                catch (Exception ex) // במקרה של שגיאה בטעינת הקובץ
+                {
+                    MessageBox.Show("Error loading XML file: " + ex.Message); // הצגת הודעת שגיאה
+                }
+            }
+            else // אם קובץ ה-XML לא קיים
+            {
+                XmlNode root = xmlDocument.CreateElement("Querys"); // יצירת אלמנט שורש "Drinks"
+
+                xmlDocument.AppendChild(root); // הוספת אלמנט השורש למסמך ה-XML
+                xmlDocument.Save(pathName); // שמירת קובץ ה-XML
+            }
+        }
+        //פונקציה לכתיבת התאריך בכתובה
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            result = xmlDocument.CreateElement("result");
+            //יצירת אלמנט בשם קווארי
+            XmlNode Query = xmlDocument.CreateElement("Query");
+            //יצירת אלמנט בשם יום
+            XmlNode day = xmlDocument.CreateElement("day");
+            //יצירת אלמנט בשם יום בחודש
+            XmlNode day_of_the_month = xmlDocument.CreateElement("day_of_the_month");
+            //יצירת אלמנט בשם חודש
+            XmlNode month = xmlDocument.CreateElement("month");
+            //יצירת אלמנט בשם שנה
+            XmlNode year = xmlDocument.CreateElement("year");
+
+
+            day.InnerText = cmbDay.Text; // קביעת יום
+            day_of_the_month.InnerText = cmbDayOfTheMonth.Text; // קביעת היום בחודש
+            month.InnerText = cmbMonth.Text; // קביעת החודש
+            year.InnerText = cmbYear.Text; // קביעת השנה
+            result.InnerText = FnDay() + FnDayMonth() + FnMonth() + FnYear();
+
+            // הוספת האלמנטים לאלמנט "query"
+            xmlDocument.FirstChild.AppendChild(Query);
+            Query.AppendChild(day);
+            Query.AppendChild(day_of_the_month);
+            Query.AppendChild(month);
+            Query.AppendChild(year);
+        }
+
+        private string FnDay()
+        {
+            string day = "";
+            switch (cmbDay.Text) // בדיקת שם התגית ועדכון המשתנה המתאים
+            {
+                case "ראשון": // 
+                    day = cmbDay.Text; 
+                    break;
+                case "שני": 
+                    day = cmbDay.Text; 
+                    break;
+                case "שלישי": 
+                    day = cmbDay.Text; 
+                    break;
+                case "רביעי": 
+                    day = cmbDay.Text; 
+                    break;
+                case "חמישי": 
+                    day = cmbDay.Text;
+                    break;
+                case "שישי": 
+                    day = cmbDay.Text; 
+                    break;
+                case "שבת":
+                    day = cmbDay.Text; 
+                    break;
+            }
+            return day;
+        }
+        private string FnDayMonth()
+        {
+            string day_of_the_month = "";
+            switch (cmbDayOfTheMonth.Text) // בדיקת שם התגית ועדכון המשתנה המתאים
+            {
+                case "1": 
+                    day_of_the_month = cmbDay.Text; 
+                    break;
+                case "2": 
+                    day_of_the_month = cmbDay.Text; 
+                    break;
+                case "3": 
+                    day_of_the_month = cmbDay.Text; 
+                    break;
+                case "4": 
+                    day_of_the_month = cmbDay.Text; 
+                    break;
+                case "5": 
+                    day_of_the_month = cmbDay.Text; 
+                    break;
+                case "6": 
+                    day_of_the_month = cmbDay.Text;
+                    break;
+                case "7": 
+                    day_of_the_month = cmbDay.Text; 
+                    break;
+                case "8":
+                    day_of_the_month = cmbDay.Text;
+                    break;
+                case "9":
+                    day_of_the_month = cmbDay.Text;
+                    break;
+                case "10":
+                    day_of_the_month = cmbDay.Text;
+                    break;
+                case "11":
+                    day_of_the_month=cmbDay.Text;
+                    break;
+
+
+
+            }
+            return day_of_the_month;
+        }
+
+        private string FnMonth()
+        {
+            return "";
+        }
+
+        private string FnYear()
+        {
+            return "";
+
+        }
+
+        // פונקציה להצגת התוצאה
+
+        // פונקציה לניקוי כל הקונטרולים
+        private void CleanAllControls()
+        {
+            foreach (Control ctr in Controls) // מעבר על כל הקונטרולים בטופס
+            {
+                if (ctr is TextBox) ctr.Text = ""; // איפוס טקסט בוקסים
+            }
+        }
+
+    }
+}
